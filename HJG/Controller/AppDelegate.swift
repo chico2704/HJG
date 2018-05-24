340211a2018009dcb9f39033b1b6fec8ec9ca956
Chico <pxxxsxzy.0@gmail.com>
2018-05-25 00:55:38 +0900

    add signIn & Out with email & google

--- HJG/Controller/AppDelegate.swift
+++ HJG/Controller/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        let db = Firestore.firestore()
         
         // 자동 로그인
         if UserDefaults.standard.bool(forKey: "googleLogIn") {
--- HJG/Controller/SignUpVC.swift
+++ HJG/Controller/SignUpVC.swift
@@ -7,32 +7,56 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class SignUpVC: UIViewController {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var pwdTF: UITextField!
+    
+    var userRef: CollectionReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        userRef = Firestore.firestore().collection(CollectionName.user.rawValue)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func closedBtnTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func signUpWithNewAccountBtnTapped(_ sender: Any) {
+        // check if all TFs have valid text value and unwrap
+        guard let name = usernameTF.text,
+            name != "",
+            let email = emailTF.text,
+            email != "",
+            let pwd = pwdTF.text,
+            pwd != ""
+            else {
+                AlertVC.showAlert(self, title: "Missing Info", message: "Please fill out all fields")
+                return
+        }
+        
+        // unless they are empty, we can move forward
+        Auth.auth().createUser(withEmail: email, password: pwd) { (user, error) in
+            guard error == nil else {
+                AlertVC.showAlert(self, title: "Error", message: error!.localizedDescription)
+                return
+            }
+            
+            guard let user = user else { return }
+            let dataToSave: [String : Any] = ["email" : email, "pwd" : pwd, "name" : name, "uid" : user.user.uid]
+            self.userRef.document(user.user.uid).setData(dataToSave) { (error) in
+                if let error = error {
+                    print("error:", error)
+                } else {
+                    print("Data has been saved!")
+                }
+            }
+        }
+        // go main tab bar
+    }
 }
